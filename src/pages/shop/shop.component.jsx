bd7c178fa5b37ad3fb7ddb2f7d81aad0997e283f
David Puscas <david.puscas93@gmail.com>
2021-05-31 23:30:50 +0300

    updated collection page, added styling to it

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,16 @@
-import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import './shop.styles.scss';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import Collection from '../collection/collection.component';
 
-import SHOP_DATA from '../../data/shop/shop.data';
+import "./shop.styles.scss";
 
-class Shop extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
-
-    render() {
-        const { collections } = this.state;
-
-        return (
-            <div className='shop'>
-                {collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                ))}
-            </div>
-        )
-    }
-}
+const Shop = ({ match }) => (
+  <div className="shop">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={Collection} />
+  </div>
+);
 
 export default Shop;
