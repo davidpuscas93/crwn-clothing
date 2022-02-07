ffa2b0f6dc3d9f087db206310124eadc7420518c
David Puscas <david.puscas93@gmail.com>
2022-02-07 19:40:11 +0200

    updated index.js to use proper service-worker registration.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import * as serviceWorker from "./service-worker";
+import * as serviceWorker from "./serviceWorkerRegistration";
 import { store, persistor } from "./redux/store";
 
 import "./index.css";
