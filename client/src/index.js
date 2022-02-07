8fb95008cb823182142885fb5daf39b23c1e1ad2
David Puscas <david.puscas93@gmail.com>
2022-02-07 19:36:58 +0200

    updated code to be PWA compliant: service-worker, updated manifest, added HTTPS.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorker from "./service-worker";
+import { store, persistor } from "./redux/store";
 
-import { store, persistor } from './redux/store';
-
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
@@ -21,13 +21,13 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
